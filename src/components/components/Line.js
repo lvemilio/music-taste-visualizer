f42a6078dc774f226a7ff3e61f6685b0a4971b4b
Emils Vigants <emils.vigants@tietoevry.com>
2023-04-11 18:00:35 +0300

    rem dead code

--- src/components/components/Line.js
+++ src/components/components/Line.js
@@ -1,12 +1,3 @@
-import styled from 'styled-components';
-
-const Line = styled.hr`
-  border: none;
-  border-top: 2px solid #1DB954;
-  margin: 20px 0;
-`;
-
-
 export const StyledLine = ({ text }) => {
     return (
         <div>

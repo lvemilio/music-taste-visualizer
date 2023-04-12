02caa9c711cd9a0d6c15e4d2e7965faa51f0b22c
Emils Vigants <emils.vigants@tietoevry.com>
2023-04-12 21:25:58 +0300

    added avg value calculation

--- src/components/PlaylistViz.js
+++ src/components/PlaylistViz.js
@@ -5,7 +5,7 @@ import HighchartsReact from 'highcharts-react-official';
 
 HighchartsHeatmap(Highcharts);
 
-const SONG_PARAM = [  
+const SONG_PARAM = [
   'Valence',
   'Energy',
   'Danceability',
@@ -13,102 +13,120 @@ const SONG_PARAM = [
 ];
 
 function parseData(playlistData) {
-    var retVal = []
-    for (var i = 0; i < playlistData.length; i++) {
-        let songBlock = [];
-        var j = 0;
-        for (let category of SONG_PARAM) {
-            songBlock = [i, j, playlistData[i][category]]
-            j++;
-            retVal.push(songBlock)
-        }
+  var retVal = []
+  for (var i = 0; i < playlistData.length; i++) {
+    let songBlock = [];
+    var j = 0;
+    for (let category of SONG_PARAM) {
+      songBlock = [i, j, playlistData[i][category]]
+      j++;
+      retVal.push(songBlock)
     }
-    console.log(retVal);
-    return retVal
+  }
+  console.log(retVal);
+  return retVal
 }
 
 
 export const PlaylistViz = ({ playlistProps }) => {
-    console.log("Op", playlistProps)
+  console.log("Op", playlistProps)
 
-const options = {
-  chart: {
-    type: 'heatmap',
-    plotBorderWidth: 1,
-  },
-  title: {
-    text: 'Heatmap of playlist song parametrs',
-  },
-  xAxis: {
-    min: 0,
-    max: playlistProps.length - 1,
-    tickInterval: 1,
-    labels: {
-      step: 1,
-      style: {
-        fontSize: '14px',
-        fontFamily: 'Open Sans',
-      },
+  const calculateAverages = () => {
+    const avgProps = {
+      avgDanceability: 0,
+      avgValence: 0,
+      avgEnergy: 0,
+      avgTempo: 0
+    }
+    playlistProps.forEach((stat) => {
+      avgProps.avgDanceability += stat.Danceability / playlistProps.length
+      avgProps.avgEnergy += stat.Energy / playlistProps.length
+      avgProps.avgValence += stat.Valence / playlistProps.length
+      avgProps.avgTempo += stat.Tempo / playlistProps.length
+    })
+
+    return avgProps
+  }
+  console.log(calculateAverages())
+
+  const options = {
+    chart: {
+      type: 'heatmap',
+      plotBorderWidth: 1,
     },
-    gridLineWidth: 0,
-    lineWidth: 0.5,
-    lineColor: 'rgba(0,0,0,0.75)',
-    tickWidth: 0,
-    tickLength: 3,
-    tickColor: 'rgba(0,0,0,0.75)',
     title: {
-      text: 'Songs',
+      text: 'Heatmap of playlist song parametrs',
     },
-  },
-  yAxis: {
-    categories: SONG_PARAM,
-    title: 'Song parameters',
-    labels: {
-      style: {
-        fontSize: '14px',
-        fontFamily: 'Open Sans',
+    xAxis: {
+      min: 0,
+      max: playlistProps.length - 1,
+      tickInterval: 1,
+      labels: {
+        step: 1,
+        style: {
+          fontSize: '14px',
+          fontFamily: 'Open Sans',
+        },
+      },
+      gridLineWidth: 0,
+      lineWidth: 0.5,
+      lineColor: 'rgba(0,0,0,0.75)',
+      tickWidth: 0,
+      tickLength: 3,
+      tickColor: 'rgba(0,0,0,0.75)',
+      title: {
+        text: 'Songs',
       },
     },
-  },
-  tooltip: {
-    formatter: function () {
-      return (
-        '<b>Title: </b>' +
-        playlistProps[this.point.x]["Title"] +
-        '<br /><b>Artist:</b> ' +
-        playlistProps[this.point.x]["Artist"] +
-        '<br />' +
-        '<b>Value:</b> ' +
-        this.point.value
-      );
+    yAxis: {
+      categories: SONG_PARAM,
+      title: 'Song parameters',
+      labels: {
+        style: {
+          fontSize: '14px',
+          fontFamily: 'Open Sans',
+        },
+      },
     },
-  },
-  colorAxis: {
-    stops: [
-      [0, 'rgba(56, 7, 84, 0.4)'],
-      [0.5, 'rgba(56, 7, 84, 0.65)'],
-      [1, 'rgba(69, 9, 104, 1)'],
+    tooltip: {
+      formatter: function () {
+        return (
+          '<b>Title: </b>' +
+          playlistProps[this.point.x]["Title"] +
+          '<br /><b>Artist:</b> ' +
+          playlistProps[this.point.x]["Artist"] +
+          '<br />' +
+          '<b>Value:</b> ' +
+          this.point.value
+        );
+      },
+    },
+    colorAxis: {
+      stops: [
+        [0, 'rgba(56, 7, 84, 0.4)'],
+        [0.5, 'rgba(56, 7, 84, 0.65)'],
+        [1, 'rgba(69, 9, 104, 1)'],
+      ],
+    },
+    series: [
+      {
+        name: 'Song data',
+        borderWidth: 0.5,
+        borderColor: 'white',
+        dataLabels: {
+          enabled: false,
+          color: '#000000',
+        },
+        data: parseData(playlistProps)
+        // [
+        //   [0, 2, 100],
+        //   [4, 3, 200],
+        //   [12, 1, 600],
+        //   [10, 0, 700],
+        // ],
+      },
     ],
-  },
-  series: [
-    {
-      name: 'Song data',
-      borderWidth: 0.5,
-      borderColor: 'white',
-      dataLabels: {
-        enabled: false,
-        color: '#000000',
-      },
-      data: parseData(playlistProps)
-      // [
-      //   [0, 2, 100],
-      //   [4, 3, 200],
-      //   [12, 1, 600],
-      //   [10, 0, 700],
-      // ],
-    },
-  ],
-};
+  };
 
   return (
     <div>

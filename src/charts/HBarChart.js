900134702273d582db7465341e7dd3fb4de62ba0
AndrisBrauers <andrisbrauers@gmail.com>
2023-04-13 04:14:44 +0300

    Should be done

--- src/charts/HBarChart.js
+++ src/charts/HBarChart.js
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-const avgData = [
-  { name: 'Danceability', value: 0.5 },
-  { name: 'Energy', value: 0.8 },
-  { name: 'Valence', value: 0.4 },
+
+const avgPartyData = [
+  { name: 'Danceability', value: 0.716 },
+  { name: 'Energy', value: 0.835 },
+  { name: 'Valence', value: 0.541 },
+  { name: 'Tempo', value: 126 },
+];
+
+const avgWorkoutData = [
+  { name: 'Danceability', value: 0.734 },
+  { name: 'Energy', value: 0.742 },
+  { name: 'Valence', value: 0.475 },
+  { name: 'Tempo', value: 128 },
+];
+
+const avgDinnerData = [
+  { name: 'Danceability', value: 0.668 },
+  { name: 'Energy', value: 0.624 },
+  { name: 'Valence', value: 0.731 },
+  { name: 'Tempo', value: 114 },
 ];
 
 const HBarChart = ({ data }) => {
@@ -12,48 +28,151 @@ const HBarChart = ({ data }) => {
     return `${(tickItem).toFixed(0)}%`;
   };
 
+  // const formatAvg = ( buttonPress ) => {
+  //   return retVal;
+  // };
+
   const modifiedData = (data.val).map((item) => ({
     ...item,
     Value: (item.value * 100).toFixed(1),
   }));
 
-  const [showAverage, setShowAverage] = useState(false);
+  const [showAverage, setShowAverage] = useState([false, false, false]);
 
-  const handleAverageClick = () => {
-    console.log("Data: ", data)
-    setShowAverage(!showAverage);
+  const handleAveragePartyClick = () => {
+    if (!showAverage[0]) {
+      setShowAverage([true, false, false]);
+    } else {
+      setShowAverage([false, false, false]);
+    }
   };
 
-  const chartData = showAverage
+  const handleAverageWorkoutClick = () => {
+    if (!showAverage[1]) {
+      setShowAverage([false, true, false]);
+    } else {
+      setShowAverage([false, false, false]);
+    }
+  };
+  const handleAverageDinnerClick = () => {
+    if (!showAverage[2]) {
+      setShowAverage([false, false, true]);
+    } else {
+      setShowAverage([false, false, false]);
+    }
+  };
+
+  const chartData = showAverage[0]
     ? modifiedData.map((item) => {
-      const avgItem = avgData.find((avg) => avg.name === item.name);
+      const avgItem = avgPartyData.find((avg) => avg.name === item.name);
       return {
         ...item,
         AvgValue: (avgItem ? avgItem.value : 0) * 100,
+        label: 'Average Value'
+      };
+    })
+    : showAverage[1] ? modifiedData.map((item) => {
+      const avgItem = avgWorkoutData.find((avg) => avg.name === item.name);
+      return {
+        ...item,
+        AvgValue: (avgItem ? avgItem.value : 0) * 100,
+        label: 'Average Value'
+      };
+    })
+    : showAverage[2] ? modifiedData.map((item) => {
+      const avgItem = avgDinnerData.find((avg) => avg.name === item.name);
+      return {
+        ...item,
+        AvgValue: (avgItem ? avgItem.value : 0) * 100,
+        label: 'Average Value'
       };
     })
     : modifiedData;
 
   return (
+
     <div style={{ position: 'relative', fontSize: '15px' }}>
-      <button onClick={handleAverageClick}>Toggle Average</button>
+    <div style={{  }}>
+      <button onClick={handleAveragePartyClick} style={{
+        backgroundColor: showAverage[0] ? '#1ED760' : 'rgb(19 132 59)',
+        color: 'white',
+        padding: '10px',
+        margin: '10px',
+        borderRadius: '5px',
+        border: 'none',
+        cursor: 'pointer',
+        outline: 'none',
+        fontWeight: 'bold',
+        transition: 'background-color 0.2s ease-in-out'
+        }}
+        onMouseOver={(e) => e.target.style.backgroundColor = '#1ED760'}
+        onMouseOut={(e) => e.target.style.backgroundColor = showAverage[0] ? '#1ED760' : 'rgb(19 132 59)'}
+        >
+        Average Party Song
+      </button>
+       <button onClick={handleAverageWorkoutClick} style={{
+        backgroundColor: showAverage[1] ? '#1ED760' : 'rgb(19 132 59)',
+        color: 'white',
+        padding: '10px',
+        margin: '10px',
+        borderRadius: '5px',
+        border: 'none',
+        cursor: 'pointer',
+        outline: 'none',
+        fontWeight: 'bold',
+        transition: 'background-color 0.2s ease-in-out'
+        }}
+        onMouseOver={(e) => e.target.style.backgroundColor = '#1ED760'}
+        onMouseOut={(e) => e.target.style.backgroundColor = showAverage[1] ? '#1ED760' : 'rgb(19 132 59)'}
+        >
+        Average Workout Song
+      </button>
+       <button onClick={handleAverageDinnerClick} style={{
+        backgroundColor: showAverage[2] ? '#1ED760' : 'rgb(19 132 59)',
+        color: 'white',
+        padding: '10px',
+        margin: '10px',
+        borderRadius: '5px',
+        border: 'none',
+        cursor: 'pointer',
+        outline: 'none',
+        fontWeight: 'bold',
+        transition: 'background-color 0.2s ease-in-out'
+        }}
+        onMouseOver={(e) => e.target.style.backgroundColor = '#1ED760'}
+        onMouseOut={(e) => e.target.style.backgroundColor = showAverage[2] ? '#1ED760' : 'rgb(19 132 59)'}
+        >
+        Average Dinner Song
+      </button>
+      </div>
       <BarChart
-        width={600}
-        height={300}
+        width={700}
+        height={320}
         data={chartData}
         layout="vertical"
-        margin={{ top: 20, right: 30, left: 30, bottom: 5 }}
+        margin={{ top: 50, right: 30, left: 30, bottom: 5 }}
       >
         <XAxis type="number" domain={[0, 100]} tickFormatter={formatPercent} />
         <YAxis dataKey="name" type="category" />
         <Tooltip />
         <Legend />
         <Bar dataKey="Value" fill="#1db954" />
-        {showAverage && <Bar dataKey="AvgValue" fill="gray" />}
+        {showAverage[0] && <Bar dataKey="AvgValue" name="Average Party Song" fill="gray" />}
+        {showAverage[1] && <Bar dataKey="AvgValue" name="Average Workout Song" fill="gray" />}
+        {showAverage[2] && <Bar dataKey="AvgValue" name="Average Dinner Song" fill="gray" />}
       </BarChart>
-      <div style={{ position: 'absolute', top: '0px', right: '10px' }}>
-        <h3>Tempo: {data.tempo} BPM</h3>
-      </div>
+      <div style={{ position: 'absolute', top: '45px', right: '10px', textAlign: 'right' }}>
+        <h3 style={{ margin: '0' }}>Tempo: {data.tempo} BPM</h3>
+        {showAverage[0] && <h3 style={{ margin: '0' }}>Average Party Song Tempo: {avgPartyData[3].value} BPM</h3>}
+      </div> 
+      <div style={{ position: 'absolute', top: '45px', right: '10px', textAlign: 'right' }}>
+        <h3 style={{ margin: '0' }}>Tempo: {data.tempo} BPM</h3>
+        {showAverage[1] && <h3 style={{ margin: '0' }}>Average Workout Song Tempo: {avgWorkoutData[3].value} BPM</h3>}
+      </div> 
+      <div style={{ position: 'absolute', top: '45px', right: '10px', textAlign: 'right' }}>
+        <h3 style={{ margin: '0' }}>Tempo: {data.tempo} BPM</h3>
+        {showAverage[2] && <h3 style={{ margin: '0' }}>Average Dinner Song Tempo: {avgDinnerData[3].value} BPM</h3>}
+      </div> 
     </div>
   );
 };
--- src/components/PlaylistViz.js
+++ src/components/PlaylistViz.js
@@ -18,18 +18,16 @@ function parseData(playlistData) {
     let songBlock = [];
     var j = 0;
     for (let category of SONG_PARAM) {
-      songBlock = [i, j, playlistData[i][category]]
+      songBlock = [i + 1, j, playlistData[i][category]]
       j++;
       retVal.push(songBlock)
     }
   }
-  console.log(retVal);
   return retVal
 }
 
 
 export const PlaylistViz = ({ playlistProps }) => {
-  console.log("Op", playlistProps)
 
   const calculateAvgStat = (stat) => {
     let avgStat = 0
@@ -39,7 +37,6 @@ export const PlaylistViz = ({ playlistProps }) => {
 
     return avgStat / playlistProps.length
   }
-  console.log(calculateAvgStat('Danceability'))
 
   const options = {
     chart: {
@@ -48,16 +45,20 @@ export const PlaylistViz = ({ playlistProps }) => {
     },
     title: {
       text: 'Heatmap of playlist song parametrs',
+      style: {
+          fontSize: '20px',
+          fontFamily: 'Baloo 2',
+        },
     },
     xAxis: {
-      min: 0,
-      max: playlistProps.length - 1,
+      min: 1,
+      max: playlistProps.length,
       tickInterval: 1,
       labels: {
         step: 1,
         style: {
           fontSize: '14px',
-          fontFamily: 'Open Sans',
+          fontFamily: 'Baloo 2',
         },
       },
       gridLineWidth: 0,
@@ -68,38 +69,75 @@ export const PlaylistViz = ({ playlistProps }) => {
       tickColor: 'rgba(0,0,0,0.75)',
       title: {
         text: 'Songs',
+        style: {
+          fontSize: '14px',
+          fontFamily: 'Baloo 2',
+        },
       },
+
     },
     yAxis: {
       categories: SONG_PARAM,
       title: 'Song parameters',
       labels: {
         style: {
-          fontSize: '14px',
-          fontFamily: 'Open Sans',
+          fontSize: '16px',
+          fontFamily: 'Baloo 2',
         },
       },
     },
     tooltip: {
       formatter: function () {
+        if (this.point.options.y == 3) {
+            this.point.value = (this.point.options.value * 200).toFixed(0) + " BPM"
+            this.point.avg = (calculateAvgStat('Tempo') * 200).toFixed(0) + " BPM"
+        } else if (this.point.options.y == 2) {
+            this.point.value = (this.point.options.value * 100).toFixed(1) + '%'
+            this.point.avg = (calculateAvgStat('Danceability') * 100).toFixed(1) + "%"
+        } else if (this.point.options.y == 1) {
+            this.point.value = (this.point.options.value * 100).toFixed(1) + '%'
+            this.point.avg = (calculateAvgStat('Energy') * 100).toFixed(1) + "%"
+        } else if (this.point.options.y == 0) {
+            this.point.value = (this.point.options.value * 100).toFixed(1) + '%'
+            this.point.avg = (calculateAvgStat('Valence') * 100).toFixed(1) + "%"
+        }
         return (
           '<b>Title: </b>' +
-          playlistProps[this.point.x]["Title"] +
+          playlistProps[this.point.x - 1]["Title"] +
           '<br /><b>Artist:</b> ' +
-          playlistProps[this.point.x]["Artist"] +
+          playlistProps[this.point.x - 1]["Artist"] +
           '<br />' +
-          '<b>Value:</b> ' +
-          this.point.value
+          '<b>Value:</b> ' + this.point.value +
+          '<br />' +
+          '<b>Average value:</b> ' + this.point.avg
         );
       },
     },
     colorAxis: {
       stops: [
-        [0, 'rgba(56, 7, 84, 0.4)'],
-        [0.5, 'rgba(56, 7, 84, 0.65)'],
-        [1, 'rgba(69, 9, 104, 1)'],
-      ],
-    },
+            [0, '#C3DAC4'],
+            [0.25, '#88B589'],
+            [0.5, '#4C904E'],
+            [0.75, '#2D692F'],
+            [1.0, '#1C411D'],
+          ],
+          labels: {
+            formatter: function() {
+              if (this.pos === 0) {
+                return '0%';
+              } else if (this.pos === 0.25) {
+                return '25%';
+              } else if (this.pos === 0.5) {
+                return '50%';
+              } else if (this.pos === 0.75) {
+                return '75%';
+              } else if (this.pos === 1.0) {
+                return '100%';
+              }
+            }
+          }
+        },
+
     series: [
       {
         name: 'Song data',
@@ -110,12 +148,6 @@ export const PlaylistViz = ({ playlistProps }) => {
           color: '#000000',
         },
         data: parseData(playlistProps)
-        // [
-        //   [0, 2, 100],
-        //   [4, 3, 200],
-        //   [12, 1, 600],
-        //   [10, 0, 700],
-        // ],
       },
     ],
   };
